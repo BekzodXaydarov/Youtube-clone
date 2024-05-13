8c2d444bb87be43231c112461e83c6a67c798ba6
Takitok2011 <bekzodeoke@gmail.com>
2024-05-13 21:37:03 +0300

    m

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./Home.css"
+import SideBar from '../../Components/SideBar/SideBar'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <SideBar />
+    </>
   )
 }
 
