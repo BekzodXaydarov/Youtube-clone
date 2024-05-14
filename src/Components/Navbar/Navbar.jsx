822f1530531f2da2753deee0aafdf8d9550164f2
Takitok2011 <bekzodeoke@gmail.com>
2024-05-14 21:47:00 +0300

    m

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,13 +7,16 @@ import upload_icon from "../../assets/upload.png"
 import more_icon from "../../assets/more.png"
 import notification_icon from "../../assets/notification.png"
 import profile_icon from "../../assets/jack.png"
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setSideBar}) => {
   return (
    <nav className='flex-div'>
       <div className="nav-left flex-div">
-        <img className='menu-icon' src={menu_icon} alt="" />
+        <img className='menu-icon' onClick={()=>setSideBar(prev=>prev===false?true:false)} src={menu_icon} alt="" />
+        <Link to={"/"}>
         <img className='logo' src={logo} alt="" />
+        </Link>
       </div>
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
